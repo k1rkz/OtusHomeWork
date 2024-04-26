ba833b96f67bc1d6578d733154f23113bb0a263a
k1rkz <gol1kov.kirill@rambler.ru>
2024-04-27 00:22:57 +0300

    first task and part in second

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,29 +4,32 @@
 """
 
 
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
+def power_numbers(*num):
+    print([n ** 2 for n in num])
 
+power_numbers(1,3,5,6)
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(i):
+    if i % i == 0 and i != 0:
+        return True
+    else:
+        return False
+def filter_numbers(*list, condition):
+    list.filter(condition == "odd" )
 
-def filter_numbers():
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
 
-    >>> filter_numbers([1, 2, 3], ODD)
+filter_numbers([1, 2, 3], ODD)
     <<< [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+filter_numbers([1, 2, 3], ODD)
